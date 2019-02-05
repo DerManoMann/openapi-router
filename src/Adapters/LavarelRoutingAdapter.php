931ecae3d50fc3ab2445fec154a2181d351c43ff
martinr <martin.rademacher@sli-systems.com>
2019-02-05 15:17:02 +1300

    Add basic implementation dealing with invoke

---
+++ src/Adapters/LavarelRoutingAdapter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Radebatz\OpenApi\Routing\Adapters;
+
+use Illuminate\Support\Facades\Route;
+use OpenApi\Annotations\Operation;
+use OpenApi\Annotations\Parameter;
+use Radebatz\OpenApi\Routing\RoutingAdapterInterface;
+
+/**
+ * Lavarel routing adapter.
+ */
+class LavarelRoutingAdapter implements RoutingAdapterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register(Operation $operation, array $parameters)
+    {
+        $path = $operation->path;
+        $operationId = str_replace('::__invoke', '', $operation->operationId);
+        $operationId = str_replace('App\\Http\\Controllers\\', '', $operationId);
+
+        /** @var Parameter $parameter */
+        foreach ($parameters as $parameter) {
+            $name = $parameter->name;
+            // TODO
+        }
+
+        Route::match($operation->method, $path, $operationId);
+    }
+}
